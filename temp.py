9a72284234ee50e1386f3129f86079d42abe30f6
Venna Bharat <142382354+vennabharat@users.noreply.github.com>
2026-06-16 21:35:39 +0530

    updated

--- temp.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-
-students_original = pd.read_csv("student_placement_analysis.csv")
-
-print(students_original.loc[len(students_original)-1:len(students_original), ["Aptitude", "Communication", "Coding"]].values)
